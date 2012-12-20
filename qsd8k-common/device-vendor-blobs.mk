fbe9f50babdc18a522ddd038597d15111a645fb3
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-19 19:53:29 -0600

    qsd8k: cleanup blobs
    Change-Id: I061b95bb37dda19ca1022653b63495be15d95c0d

--- qsd8k-common/device-vendor-blobs.mk
+++ qsd8k-common/device-vendor-blobs.mk
@@ -27,13 +27,3 @@ PRODUCT_COPY_FILES += \
 # Omx
 PRODUCT_COPY_FILES += \
     vendor/htc/qsd8k-common/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so
-
-# Vendor library
-PRODUCT_COPY_FILES += \
-    vendor/htc/qsd8k-common/proprietary/libqc-opt.so:system/lib/libqc-opt.so
-
-# Media
-PRODUCT_COPY_FILES += \
-    vendor/htc/qsd8k-common/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/htc/qsd8k-common/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    vendor/htc/qsd8k-common/proprietary/libwvm.so:system/vendor/lib/libwvm.so
