42f59f2ab604e969d601bfd719c79ec8df81d173
helicopter88 <johnlemon.lol@gmail.com>
2013-01-13 08:34:58 -0700

    shooteru: Add firmwares
    Change-Id: Id19a4e054d08ec3df47614a45323c76bcc0fc7a3
    Signed-off-by: helicopter88 <johnlemon.lol@gmail.com>

--- shooteru/shooteru-vendor-blobs.mk
+++ shooteru/shooteru-vendor-blobs.mk
@@ -105,6 +105,29 @@ PRODUCT_COPY_FILES +=  \
     vendor/htc/shooteru/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
     vendor/htc/shooteru/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
 
+# Firmwares
+PRODUCT_COPY_FILES += \
+    vendor/htc/shooteru/proprietary/firmware/modem.b00:/system/vendor/firmware/modem.b00 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b01:/system/vendor/firmware/modem.b01 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b02:/system/vendor/firmware/modem.b02 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b03:/system/vendor/firmware/modem.b03 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b04:/system/vendor/firmware/modem.b04 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b05:/system/vendor/firmware/modem.b05 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b06:/system/vendor/firmware/modem.b06 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b07:/system/vendor/firmware/modem.b07 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b08:/system/vendor/firmware/modem.b08 \
+    vendor/htc/shooteru/proprietary/firmware/modem.b09:/system/vendor/firmware/modem.b09 \
+    vendor/htc/shooteru/proprietary/firmware/modem.mdt:/system/vendor/firmware/modem.mdt \
+    vendor/htc/shooteru/proprietary/firmware/q6.b00:/system/vendor/firmware/q6.b00 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b01:/system/vendor/firmware/q6.b01 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b02:/system/vendor/firmware/q6.b02 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b03:/system/vendor/firmware/q6.b03 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b04:/system/vendor/firmware/q6.b04 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b05:/system/vendor/firmware/q6.b05 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b06:/system/vendor/firmware/q6.b06 \
+    vendor/htc/shooteru/proprietary/firmware/q6.b07:/system/vendor/firmware/q6.b07 \
+    vendor/htc/shooteru/proprietary/firmware/q6.mdt:/system/vendor/firmware/q6.mdt
+
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
