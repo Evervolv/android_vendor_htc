8cab8c0a6475bb01ae2412e963e48b77a8ebf0b7
Nick Reuter <nreuter85@gmail.com>
2013-03-07 22:50:24 -0600

    Glacier: Update props
    Change-Id: Ic519f7d48db32d9f4f5c5cfd4b77197d7b086edd

--- glacier/device-vendor-blobs.mk
+++ glacier/device-vendor-blobs.mk
@@ -16,17 +16,24 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-  vendor/htc/glacier/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
+  vendor/htc/glacier/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
+  vendor/htc/glacier/proprietary/lib/libv8.so:obj/lib/libv8.so \
+  vendor/htc/glacier/proprietary/lib/libqc-opt.so:obj/lib/libqc-opt.so
 
 PRODUCT_COPY_FILES += \
-  vendor/htc/glacier/proprietary/etc/vpimg:system/etc/vpimg \
   vendor/htc/glacier/proprietary/bin/akmd:system/bin/akmd \
   vendor/htc/glacier/proprietary/bin/awb_camera:system/bin/awb_camera \
-  vendor/htc/glacier/proprietary/bin/bma150_usr:system/bin/bma150_usr \
   vendor/htc/glacier/proprietary/bin/lsc_camera:system/bin/lsc_camera \
   vendor/htc/glacier/proprietary/bin/rmt_storage:system/bin/rmt_storage \
   vendor/htc/glacier/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
   vendor/htc/glacier/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_command_control.fw:system/etc/firmware/vidc_720p_command_control.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_h263_dec_mc.fw:system/etc/firmware/vidc_720p_h263_dec_mc.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_h264_dec_mc.fw:system/etc/firmware/vidc_720p_h264_dec_mc.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_h264_enc_mc.fw:system/etc/firmware/vidc_720p_h264_enc_mc.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
   vendor/htc/glacier/proprietary/lib/hw/camera.default.so:system/lib/hw/camera.default.so \
   vendor/htc/glacier/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
   vendor/htc/glacier/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
@@ -42,10 +49,33 @@ PRODUCT_COPY_FILES += \
   vendor/htc/glacier/proprietary/lib/libwvdrm_L3.so:system/lib/libwvdrm_L3.so \
   vendor/htc/glacier/proprietary/lib/libwvm.so:system/lib/libwvm.so \
   vendor/htc/glacier/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
+  vendor/htc/glacier/proprietary/lib/libv8.so:system/lib/libv8.so \
+  vendor/htc/glacier/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+  vendor/htc/glacier/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+  vendor/htc/glacier/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
+  vendor/htc/glacier/proprietary/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
+  vendor/htc/glacier/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
+  vendor/htc/glacier/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
+  vendor/htc/glacier/proprietary/lib/libcneapiclient.so:system/lib/libcneapiclient.so \
+  vendor/htc/glacier/proprietary/lib/libcneqmiutils.so:system/lib/libcneqmiutils.so \
+  vendor/htc/glacier/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+  vendor/htc/glacier/proprietary/lib/libdnshostprio.so:system/lib/libdnshostprio.so \
+  vendor/htc/glacier/proprietary/lib/libnetmonitor.so:system/lib/libnetmonitor.so \
+  vendor/htc/glacier/proprietary/lib/libtcpfinaggr.so:system/lib/libtcpfinaggr.so \
+  vendor/htc/glacier/proprietary/lib/pp_proc_plugin.so:system/lib/pp_proc_plugin.so \
+  vendor/htc/glacier/proprietary/lib/qnet-plugin.so:system/lib/qnet-plugin.so \
+  vendor/htc/glacier/proprietary/lib/tcp-connections.so:system/lib/tcp-connections.so \
+  vendor/htc/glacier/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+  vendor/htc/glacier/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+  vendor/htc/glacier/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
   vendor/htc/glacier/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
   vendor/htc/glacier/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+  vendor/htc/glacier/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
   vendor/htc/glacier/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
   vendor/htc/glacier/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+  vendor/htc/glacier/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
   vendor/htc/glacier/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
   vendor/htc/glacier/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
   vendor/htc/glacier/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
