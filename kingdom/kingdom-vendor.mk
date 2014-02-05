f60f8b0c63dd8030bdab5d895189a540a2fcc974
Alex Page <Frigids@gmail.com>
2014-02-04 22:31:49 -0800

    Kingdom: Remove common and unnecessary props
    Change-Id: I46196ecfe731b9e233a1b1e71e9a4aad6ec72842

--- kingdom/kingdom-vendor.mk
+++ kingdom/kingdom-vendor.mk
@@ -18,12 +18,12 @@
 PRODUCT_PACKAGES := \
     LiveWallpapers \
     LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
     librs_jni
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
-
-# Pick up overlay for features that depend on non-open-source files
 
 $(call inherit-product, vendor/htc/kingdom/kingdom-vendor-blobs.mk)
